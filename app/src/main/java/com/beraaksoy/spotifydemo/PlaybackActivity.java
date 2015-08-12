cd397a159f13b5a1aafe5e2b1cc33214720d6dcb
baksoy <baksoy@gmail.com>
2015-08-11 22:46:42 -0400

    Add PlaybackActivity and bind it to the toptracks_playback.xml UI

---
+++ app/src/main/java/com/beraaksoy/spotifydemo/PlaybackActivity.java
@@ -0,0 +1,14 @@
+package com.beraaksoy.spotifydemo;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class PlaybackActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.toptracks_playback);
+    }
+}
