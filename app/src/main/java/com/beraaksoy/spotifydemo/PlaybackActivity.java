5f2751f92e49262d5fdeeea057063a0ccf79d7d6
baksoy <baksoy@gmail.com>
2015-08-17 18:45:37 -0400

    New playback interrupting another playback without crash. Seekbar shows progress and resets to the beginning at the end of the playback

--- app/src/main/java/com/beraaksoy/spotifydemo/PlaybackActivity.java
+++ app/src/main/java/com/beraaksoy/spotifydemo/PlaybackActivity.java
@@ -1,6 +1,8 @@
 package com.beraaksoy.spotifydemo;
 
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,25 +16,50 @@ import android.widget.SeekBar;
 
 import com.squareup.picasso.Picasso;
 
-public class PlaybackActivity extends ActionBarActivity implements View.OnClickListener, View.OnTouchListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnBufferingUpdateListener {
+public class PlaybackActivity extends ActionBarActivity implements View.OnTouchListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnBufferingUpdateListener {
 
-    private ImageButton mButtonPlayPause;
     private SeekBar mSeekBarProgress;
     private MediaPlayer mMediaPlayer;
-    private int mMediaFileLengthInMilliseconds; // this value contains the song duration in milliseconds. Look at getDuration() method in MediaPlayer class
     private final Handler mHandler = new Handler();
     private String mSongPreviewUrl;
     private String mPreviewAlbumImgUrl;
-    private ImageView mPreviewAlbumImg;
+    private ImageButton mPlaypauseButton;
+    private int mMediaFileLengthInMilliseconds;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.toptracks_playback);
         Bundle extras = getIntent().getExtras();
         mSongPreviewUrl = extras.getString("playback_url");
         mPreviewAlbumImgUrl = extras.getString("preview_album_img");
+        final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
         initView();
+
+        mPlaypauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setupMedia();
+                if (!mMediaPlayer.isPlaying()) {
+                    playMedia();
+                } else {
+                    pauseMedia();
+                }
+            }
+        });
+
+
+        audioManager.requestAudioFocus(new AudioManager.OnAudioFocusChangeListener() {
+            @Override
+            public void onAudioFocusChange(int i) {
+                if (i == AudioManager.AUDIOFOCUS_LOSS) {
+                    mMediaPlayer.stop();
+                    audioManager.abandonAudioFocus(this);
+                }
+            }
+        }, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
     }
 
     /**
@@ -40,9 +67,8 @@ public class PlaybackActivity extends ActionBarActivity implements View.OnClickL
      */
     private void initView() {
 
-        mPreviewAlbumImg = (ImageView) findViewById(R.id.previewAlbumImg);
-        mButtonPlayPause = (ImageButton) findViewById(R.id.buttonPlayPause);
-        mButtonPlayPause.setOnClickListener(this);
+        ImageView previewAlbumImg = (ImageView) findViewById(R.id.previewAlbumImg);
+        mPlaypauseButton = (ImageButton) findViewById(R.id.buttonPlayPause);
 
         mSeekBarProgress = (SeekBar) findViewById(R.id.seekBarPreview);
         mSeekBarProgress.setMax(99); // It means 100% .0-99
@@ -51,51 +77,66 @@ public class PlaybackActivity extends ActionBarActivity implements View.OnClickL
         mMediaPlayer = new MediaPlayer();
         mMediaPlayer.setOnBufferingUpdateListener(this);
         mMediaPlayer.setOnCompletionListener(this);
+
+
         Log.i("PLAYBACK_URL", mPreviewAlbumImgUrl);
 
         Picasso.with(getApplicationContext())
                 .load(mPreviewAlbumImgUrl)
                 .resize(640, 640)
                 .centerInside()
-                .into(mPreviewAlbumImg);
+                .into(previewAlbumImg);
     }
 
     /**
      * Method which updates the SeekBar primary progress by current song playing position
      */
+//    private void seekBarProgressUpdater() {
+//        mSeekBarProgress.setProgress((int) (((float) mMediaPlayer.getCurrentPosition() / mMediaPlayer.getDuration()) * 100)); // This math construction give a percentage of "was playing"/"song length"
+//        Runnable notification = new Runnable() {
+//            public void run() {
+//                seekBarProgressUpdater();
+//            }
+//        };
+//        mHandler.postDelayed(notification, 1000);
+//    }
     private void primarySeekBarProgressUpdater() {
+        mMediaFileLengthInMilliseconds = mMediaPlayer.getDuration(); // get the song length in milliseconds from URL
         mSeekBarProgress.setProgress((int) (((float) mMediaPlayer.getCurrentPosition() / mMediaFileLengthInMilliseconds) * 100)); // This math construction give a percentage of "was playing"/"song length"
-        if (mMediaPlayer.isPlaying()) {
+
+        if (mMediaPlayer.isPlaying() || mMediaPlayer.getCurrentPosition() < mMediaFileLengthInMilliseconds - 1000) {
             Runnable notification = new Runnable() {
                 public void run() {
                     primarySeekBarProgressUpdater();
                 }
             };
             mHandler.postDelayed(notification, 1000);
+        } else {
+            //Reset seekbar to the beginning
+            mSeekBarProgress.setProgress(0);
         }
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.buttonPlayPause) {
-            /** ImageButton onClick event handler. Method which start/pause mediaplayer playing */
-            try {
-                mMediaPlayer.setDataSource(mSongPreviewUrl); // setup song to mediaplayer data source
-                mMediaPlayer.prepare(); // you must call this method after setup the datasource in setDataSource method. After calling prepare() the instance of MediaPlayer starts load data from URL to internal buffer.
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    private void pauseMedia() {
+        mMediaPlayer.pause();
+        mPlaypauseButton.setImageResource(android.R.drawable.ic_media_play);
+    }
 
-            mMediaFileLengthInMilliseconds = mMediaPlayer.getDuration(); // get the song length in milliseconds from URL
+    private void playMedia() {
+        mMediaPlayer.start();
+        mPlaypauseButton.setImageResource(android.R.drawable.ic_media_pause);
+        //seekBarProgressUpdater();
+        primarySeekBarProgressUpdater();
+    }
 
-            if (!mMediaPlayer.isPlaying()) {
-                mMediaPlayer.start();
-                mButtonPlayPause.setImageResource(android.R.drawable.ic_media_pause);
-            } else {
-                mMediaPlayer.pause();
-                mButtonPlayPause.setImageResource(android.R.drawable.ic_media_play);
-            }
-            primarySeekBarProgressUpdater();
+    private void setupMedia() {
+        /* Setting up the media to be played */
+        try {
+            mMediaPlayer.setDataSource(mSongPreviewUrl); // setup song to mediaplayer data source
+            mMediaPlayer.prepare(); // you must call this method after setup the datasource in setDataSource method. After calling prepare() the instance of MediaPlayer starts load data from URL to internal buffer.
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -105,7 +146,7 @@ public class PlaybackActivity extends ActionBarActivity implements View.OnClickL
             /** Seekbar onTouch event handler. Method which seeks MediaPlayer to seekBar primary progress position*/
             if (mMediaPlayer.isPlaying()) {
                 SeekBar sb = (SeekBar) v;
-                int playPositionInMillisecconds = (mMediaFileLengthInMilliseconds / 100) * sb.getProgress();
+                int playPositionInMillisecconds = (mMediaPlayer.getDuration() / 100) * sb.getProgress();
                 mMediaPlayer.seekTo(playPositionInMillisecconds);
             }
         }
@@ -115,7 +156,7 @@ public class PlaybackActivity extends ActionBarActivity implements View.OnClickL
     @Override
     public void onCompletion(MediaPlayer mp) {
         /** MediaPlayer onCompletion event handler. Method which calls when song playing is complete*/
-        mButtonPlayPause.setImageResource(android.R.drawable.ic_media_play);
+        mPlaypauseButton.setImageResource(android.R.drawable.ic_media_play);
     }
 
     @Override
@@ -123,4 +164,5 @@ public class PlaybackActivity extends ActionBarActivity implements View.OnClickL
         /** Method which updates the SeekBar secondary progress by current song loading from URL position*/
         mSeekBarProgress.setSecondaryProgress(percent);
     }
+
 }
