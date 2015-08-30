6266fe9a85451e3ed5b8b8bc0475aa9b7c2d4acc
baksoy <baksoy@gmail.com>
2015-08-30 03:45:30 -0400

    Implement DialogFragment. On phone playback displays on new screen. On Tablet, playback sshows are foreground.Still need to fix back button going to a blank screen

--- app/src/main/java/com/beraaksoy/spotifystreamer/PlaybackActivity.java
+++ app/src/main/java/com/beraaksoy/spotifystreamer/PlaybackActivity.java
@@ -2,28 +2,43 @@ package com.beraaksoy.spotifystreamer;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class PlaybackActivity extends Activity {
+
+    boolean mIsLargeLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.toptracks_playback_activity);
 
+        mIsLargeLayout = getResources().getBoolean(R.bool.large_layout);
+
         if (savedInstanceState == null) {
-            PlaybackFragment fragment = new PlaybackFragment();
-
-            //Receive Top Tracks info via Intent
-            Bundle extras = getIntent().getExtras();
-
-            fragment.setArguments(extras);
-
-            getFragmentManager().beginTransaction()
-                    .replace(R.id.toptracks_playback_fragment, fragment)
-                    .commit();
+            showDialog();
         }
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void showDialog() {
+        //Receive Top Tracks info via Intent
+        Bundle extras = getIntent().getExtras();
+
+        FragmentManager fragmentManager = getFragmentManager();
+        PlaybackFragment fragment = new PlaybackFragment();
+
+        fragment.setArguments(extras);
+
+        if (mIsLargeLayout) {
+            fragment.show(fragmentManager, "dialog");
+        } else {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            transaction.replace(R.id.playback_fragment, fragment).addToBackStack(null).commit();
+        }
     }
 }
